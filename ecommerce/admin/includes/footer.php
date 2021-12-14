4b4e29c53b8763828c6e93ee9aad81218f3dd9be
Karim-ayed <94021765+Karim-ayed@users.noreply.github.com>
2021-12-15 00:31:15 +0100

    commit

---
+++ ecommerce/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021 Brought to You By <a href="https://katana-tech.org">Katana-Tech</a></strong>
+</footer>
---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2021 Brought to You By <a href="https://katana-tech.org">Katana-Tech</a></strong>
+    </div>
+</footer>
